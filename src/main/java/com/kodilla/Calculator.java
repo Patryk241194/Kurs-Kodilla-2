c2c5bec9661be9168efb5abd741378884ba90661
Patryk Potrykus <potrykus.patrykk@gmail.com>
2023-03-25 13:41:47 +0100

    nasza zmiana

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -10,12 +10,25 @@ public class Calculator {
         return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("You cannot divide by zero");
+        }
+        return a / b;
+    }
+
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(2,5));
         System.out.println(calculator.subtract(5,2));
+        System.out.println(calculator.multiply(5,2));
+        System.out.println(calculator.divide(5,2));
 
     }
 }
